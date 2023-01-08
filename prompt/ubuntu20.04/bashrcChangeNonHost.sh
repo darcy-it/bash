e65d5802791732031c01b14b9a67f3d801fe2ca6
darcy <info@darcy-it.com>
2023-01-08 15:53:55 +0900

    prompt

--- prompt/ubuntu20.04/bashrcChangeNonHost.sh
+++ prompt/ubuntu20.04/bashrcChangeNonHost.sh
@@ -2,6 +2,8 @@
 #
 # file: 
 
+# os/bash version
+
 #
 # be after. yes/no.
 #
@@ -12,7 +14,9 @@ echo "" && echo ""
 
 read -p "ok? (y/N): " yn
 case "$yn" in
-  [nN]) exit 0;;
+  [nN]) 
+    echo "abort"
+    exit 0;;
   [yY]) ;;
   *)
     echo "abort"
@@ -20,25 +24,43 @@ case "$yn" in
     ;;
 esac
 
-echo "Yes"
-
-# read -n1 -p "ok? (y/N): " yn
-# if [[ $yn = [yY] ]]; then
-#   echo hello
-# else
-#   echo abort
-# fi
-
-exit 0
-
 #
 # backup bashrc
 #
-mkdir -p ~./bk
+mkdir -p ~/.bk/bashrc
+# cp -p ~/.bashrc ~/.bk/bashrc/bashrc.`date +%Y.%m%d`
+# cp -p ~/.bashrc ~/.bk/bashrc/bashrc.`date +%Y.%m%d.%H%M%S`
+cp -p ~/.bashrc ~/.bk/bashrc/bashrc.`date +%Y.%m%d.%H%M`
+cp -p ~/.bashrc ~/.bk/bashrc/bashrc.latest
 
-cd 
-exit 0
+#
+# Confirmation of settings
+#
+echo ""
+echo "$ ls -l ~/.bk/bashrc"
+ls -l ~/.bk/bashrc
 
-cp -p bashrc-non-host ~/.bashrc
+#
+# setting ~/.bashrc
+#
+# exit 0
+cp -p ./bashrc/bashrc-non-host ~/.bashrc
+# delete line 60
+# cat $OUTPUTFILE | (rm $OUTPUTFILE; grep -v -e 60 > $OUTPUTFILE)
+# TMP=" \ \ \ \PS1=\'\$\{debian_chroot:+\(\$debian_chroot\)}\\\[\\\033[01\;32m\\\]\\\u@\\\h\\\[\\\033[00m\\\]:\\\[\\\033[01\;34m\\\]\\\w\\\[\\\033[00m\\\]\\\\$ "
+# sed -i -e "60i $TMP" $OUTPUTFILE
 
+#----------------------------------------
+#　Confirmation after setup
+#----------------------------------------
+diff ~/.bashrc ~/.bk/bashrc/bashrc.latest
+
+#----------------------------------------
+# Configuring Reflection
+#----------------------------------------
 exec bash
+# sh ~/.bashrc # NG
+# . ~/.bashrc # NG
+# source ~/.bashrc # NG
+
+exit 0
--- sed-input-string-shellfile/input-string-to-bashfile.sh
+++ sed-input-string-shellfile/input-string-to-bashfile.sh
@@ -34,7 +34,7 @@ done
 cat $OUTPUTFILE | (rm $OUTPUTFILE; grep -v -e 2 -e 5 > $OUTPUTFILE)
 
 #         PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
-TMP=" \ \ PS1=\'\$\{debian_chroot:+\(\$debian_chroot\)}\\\[\\\033[01\;32m\\\]\\\u@\\\h\\\[\\\033[00m\\\]:\\\[\\\033[01\;34m\\\]\\\w\\\[\\\033[00m\\\]\\\\$ "
+TMP=" \ \ \ \PS1=\'\$\{debian_chroot:+\(\$debian_chroot\)}\\\[\\\033[01\;32m\\\]\\\u@\\\h\\\[\\\033[00m\\\]:\\\[\\\033[01\;34m\\\]\\\w\\\[\\\033[00m\\\]\\\\$ "
 sed -i -e "2i $TMP" $OUTPUTFILE
 # sed -i -e "3i h" $OUTPUTFILE
 # sed -i -e "3a haa" $OUTPUTFILE
